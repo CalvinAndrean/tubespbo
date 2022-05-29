2cd2e6efc350a34933b1c0e9ec0b3a8fd1c8292e
CalvinAndrean <calvinandrean456@gmail.com>
2022-05-29 15:19:52 +0700

    aa
    a

--- TugasBesarJava/src/connection/DbConnection.java
+++ TugasBesarJava/src/connection/DbConnection.java
@@ -43,9 +43,7 @@ public class DbConnection {
             System.out.println("success!");
         } catch (Exception e) {
             System.out.println("Error closing database..");
-            System.out.println("info");
             System.out.println(e.toString());
-            System.out.println("infooooo");
         }
     }
 }
